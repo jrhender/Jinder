e6380474e3e2c579e97bd8ac02581ccd28b01605
jrhender <johnhenderson45@gmail.com>
2018-11-02 21:14:04 -0700

    Put in react-firebaseui instead of firebaseui in Login component  https://github.com/firebase/firebaseui-web-react

--- wwwroot/client/app/Login.jsx
+++ wwwroot/client/app/Login.jsx
@@ -1,55 +1,30 @@
 import React from 'react';
 import firebase from '../../firebaseInitialization';
-import * as firebaseui from 'firebaseui';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+    signInFlow: 'popup',
+    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+    signInSuccessUrl: '/signedIn',
+    // We will display Google and Facebook as auth providers.
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ]
+  };
 
 class Login extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    // FirebaseUI config.
-    var uiConfig = {
-        signInOptions: [
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-        ],
-        // tosUrl and privacyPolicyUrl accept either url string or a callback
-        // function.
-        // Terms of service url/callback.
-        tosUrl: '<your-tos-url>',
-        // Privacy policy url/callback.
-        privacyPolicyUrl: function() {
-          window.location.assign('<your-privacy-policy-url>');
-        }
-      };
-
-      // Initialize the FirebaseUI Widget using Firebase.
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-      ui.start('#firebaseui-auth-container', uiConfig);
-  }
-
-
-
-  login() {
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-      });
-  }
-
-  render() {
-    return (
-      <div id="firebaseui-auth-container"/>
-    );
-  }
+    render() {
+        return (
+          <div>
+            <h1>Welcome To Jinder!</h1>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+          </div>
+        );
+    }
 }
 
 Login.propTypes = {
-  onClose: React.PropTypes.func.isRequired,
-  show: React.PropTypes.bool,
-  children: React.PropTypes.node
 };
 
 export default Login;
