b02e52d3f5c1896e0a94f7b4394c3cd3921f64d4
jrhender <johnhenderson45@gmail.com>
2018-11-02 21:41:49 -0700

    Added isSignedIn state to Login component

--- wwwroot/client/app/Login.jsx
+++ wwwroot/client/app/Login.jsx
@@ -5,8 +5,6 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 const uiConfig = {
     // Popup signin flow rather than redirect flow.
     signInFlow: 'popup',
-    // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: '/signedIn',
     // We will display Google and Facebook as auth providers.
     signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID
@@ -14,12 +12,43 @@ const uiConfig = {
   };
 
 class Login extends React.Component {
+
+    constructor() {
+        super();
+
+        // The component's Local state.
+        this.state = {
+            isSignedIn: false // Local signed-in state.
+        };
+    }
+
+    // Listen to the Firebase Auth state and set the local state.
+    componentDidMount() {
+        this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+            (user) => this.setState({isSignedIn: !!user})
+        );
+    }
+
+    // Make sure we un-register Firebase observers when the component unmounts.
+    componentWillUnmount() {
+        this.unregisterAuthObserver();
+    }
+
     render() {
+        if (!this.state.isSignedIn) {
+            return (
+            <div>
+                <h1>Welcome To Jinder!</h1>
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
+            </div>
+            );
+        }
         return (
-          <div>
-            <h1>Welcome To Jinder!</h1>
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
-          </div>
+            <div>
+                <h1>My App</h1>
+                <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+                <a onClick={() => firebase.auth().signOut()}>Sign-out</a>
+            </div>
         );
     }
 }
