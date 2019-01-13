c49f22e91b7f95d86c575cc9f02c01c3771f9940
jrhender <johnhenderson45@gmail.com>
2019-01-13 11:42:01 -0800

    deleted unused imageService method

--- client/app/Services/paneImageService.ts
+++ client/app/Services/paneImageService.ts
@@ -1,21 +1,6 @@
 import {firestoreDB} from './firebaseInitialization';
 import firebase from './firebaseInitialization';
 
-const getPaneImageUrl = (paneNumber : number) => {
-    // Points to the root reference
-    let storageRef = firebase.storage().ref();
-
-    // Points to 'images'
-    let paneImagesRef = storageRef.child('paneImages');
-
-    // Points to 'images/space.jpg'
-    // Note that you can use variables to create child values
-    let fileName : string = 'pane0.jpg';
-    let paneRef = paneImagesRef.child(fileName);
-    let downloadURLPromise : Promise<string> = paneRef.getDownloadURL();
-    return downloadURLPromise;
-}
-
 const getProfileImagesOfCurrentUser = async () => {
     let images : object[] = new Array();
     let currentUserID : string = firebase.auth().currentUser.uid;
