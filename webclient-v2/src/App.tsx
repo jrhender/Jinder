79d2794d31b47c7c55dbf874ea74dc351cc96168
John Henderson <john.henderson@energyweb.org>
2022-01-22 12:39:48 +0100

    install react-modal

--- webclient-v2/src/App.tsx
+++ webclient-v2/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from 'react-modal';
 import './App.css';
 import Advanced from './Advanced';
 
@@ -6,6 +7,24 @@ function App() {
   return (
     <div className="App">
       <Advanced />
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
     </div>
   );
 }
